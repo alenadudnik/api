1047a5a37a46e21945a92ece8459116154be277f
Alena <7182001562alena@gmail.com>
2022-02-02 16:33:37 -0500

    add auth tests

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,26 +1,26 @@
-import supertest from 'supertest';
-const expect = require('chai').expect;
+import supertest from 'supertest'
+import { expect } from "chai";
 
-describe('auth', function() {
-    const request = supertest(process.env.BASE_URL);
+describe('AUTH LOG IN', function () {
+    const request = supertest('http://paysis.herokuapp.com')
 
-    it('successful log in', function() {
+    it('should successful log in', function () {
         request
             .post('/auth')
-            .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-            .end(function(err, res) {
-                expect(res.statusCode).to.eq(200);
-                expect(res.body.token).not.to.be.undefined;
-            });
+            .send({login: 'adminius', password: 'supers3cret'})
+            .end(function (err, res) {
+                expect(res.statusCode).to.eq(200)
+                expect(res.body.token).not.to.be.undefined
+            })
     });
 
-    it('log in with wrong credentials should return error', function() {
+    it('should unsuccessful log in', function () {
         request
             .post('/auth')
-            .send({ login: 'wrong', password: 'wrong' })
-            .end(function(err, res) {
-                expect(res.statusCode).to.eq(404);
-                expect(res.body.message).to.eq('Wrong login or password.');
-            });
+            .send({login: 'invalid', password: 'invalid'})
+            .end(function (err, res) {
+                expect(res.statusCode).to.eq(404)
+                expect(res.body.message).to.eq('Wrong login or password.')
+            })
     });
-});
+})
